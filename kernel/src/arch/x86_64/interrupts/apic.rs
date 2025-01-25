0a8ca37707c4238a31be4ea3f854a7f8a4d0b96b
Roxve <116169309+Roxve@users.noreply.github.com>
2025-01-25 13:43:18 +0200

    [Kernel x86_64]: made the LAPIC tick once every 5 ms instead of 10 ms (context switching)

--- kernel/src/arch/x86_64/interrupts/apic.rs
+++ kernel/src/arch/x86_64/interrupts/apic.rs
@@ -159,7 +159,7 @@ fn enable_apic_timer(local_apic_addr: VirtAddr, apic_id: u8) {
         core::ptr::write_volatile(addr, timer.encode_u32());
         core::ptr::write_volatile(divide, 0x3);
 
-        core::ptr::write_volatile(init, apic_timer_ms_to_ticks(10));
+        core::ptr::write_volatile(init, apic_timer_ms_to_ticks(5));
     }
 }
 
