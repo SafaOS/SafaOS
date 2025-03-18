01637adbd07ed8a70ba2f9ead04b0990397931a9
ObserverUnit <116169309+ObserverUnit@users.noreply.github.com>
2025-03-18 14:40:09 +0200

    [utils errors]: implemented as_str for ErrorStatus

--- utils/src/errors.rs
+++ utils/src/errors.rs
@@ -40,7 +40,38 @@ pub enum ErrorStatus {
     NotEnoughArguments = 0x16,
     OutOfMemory = 0x17,
 }
-
+impl ErrorStatus {
+    #[inline(always)]
+    /// Gives a string description of the error
+    pub fn as_str(&self) -> &'static str {
+        use ErrorStatus::*;
+        match *self {
+            Generic => "Generic Error",
+            OperationNotSupported => "Operation Not Supported",
+            NotSupported => "Object Not Supported",
+            Corrupted => "Corrupted",
+            InvaildSyscall => "Invaild Syscall",
+            InvaildResource => "Invaild Resource",
+            InvaildPid => "Invaild PID",
+            InvaildOffset => "Invaild Offset",
+            InvaildPtr => "Invaild Ptr (not aligned or null)",
+            InvaildStr => "Invaild Str (not utf8)",
+            StrTooLong => "Str too Long",
+            InvaildPath => "Invaild Path",
+            NoSuchAFileOrDirectory => "No Such a File or Directory",
+            NotAFile => "Not a File",
+            NotADirectory => "Not a Directory",
+            AlreadyExists => "Already Exists",
+            NotExecutable => "Not Executable",
+            DirectoryNotEmpty => "Directory not Empty",
+            MissingPermissions => "Missing Permissions",
+            MMapError => "Memory Map Error (most likely out of memory)",
+            Busy => "Resource Busy",
+            NotEnoughArguments => "Not Enough Arguments",
+            OutOfMemory => "Out of Memory",
+        }
+    }
+}
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum SysResult {
     Sucess,
