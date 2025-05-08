3ddf18f8d775620f1a4c62a12b01738df497975c
safiworks <116169309+safiworks@users.noreply.github.com>
2025-05-08 19:19:21 +0300

    [kernel build]: made it so the kernel chooses different linker scripts based on the arch.

--- crates/kernel/build.rs
+++ crates/kernel/build.rs
@@ -1,4 +1,5 @@
 fn main() {
-    println!("cargo:rustc-link-arg=-Tlinker.ld");
-    println!("cargo:rerun-if-changed=linker.ld");
+    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").expect("failed to get target arch");
+    println!("cargo:rustc-link-arg=-Tlinker-{}.ld", target_arch);
+    println!("cargo:rerun-if-changed=linker-*.ld");
 }
