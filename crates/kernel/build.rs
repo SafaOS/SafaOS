455ab457cdb7faab68b804c193d5e7364f80b7a0
safiworks <116169309+safiworks@users.noreply.github.com>
2025-05-11 04:22:27 +0300

    [kernel Cargo.toml]: added compiler builtins as a dep and a minor build.rs change.

--- crates/kernel/build.rs
+++ crates/kernel/build.rs
@@ -1,5 +1,5 @@
 fn main() {
     let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").expect("failed to get target arch");
-    println!("cargo:rustc-link-arg=-Tlinker-{}.ld", target_arch);
-    println!("cargo:rerun-if-changed=linker-*.ld");
+    println!("cargo:rustc-link-arg=-Tlinker-{target_arch}.ld");
+    println!("cargo:rerun-if-changed=linker-{target_arch}.ld");
 }
