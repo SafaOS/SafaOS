291940c52601d4dee01c10a7be4d50d6308802f2
safiworks <116169309+safiworks@users.noreply.github.com>
2026-07-24 01:10:12 +0300

    fix(xhci::extended_caps): some typos

--- safa-core/src/drivers/xhci/extended_caps.rs
+++ safa-core/src/drivers/xhci/extended_caps.rs
@@ -1,6 +1,6 @@
 use bitfield_struct::bitfield;
 
-use crate::drivers::pci::extended_caps::{ExtendedCaptability, GenericCaptability};
+use crate::drivers::pci::extended_caps::{ExtendedCapability, GenericCapability};
 
 #[bitfield(u32)]
 struct XHCIUSBSupportedCapD3 {
@@ -13,7 +13,7 @@ struct XHCIUSBSupportedCapD3 {
 #[repr(C)]
 pub struct XHCIUSBSupportedProtocolCap {
     // dword 0
-    header: GenericCaptability,
+    header: GenericCapability,
     minor_revision_version: u8,
     major_revision_version: u8,
     // dword 1
@@ -26,28 +26,28 @@ pub struct XHCIUSBSupportedProtocolCap {
     dword3: XHCIUSBSupportedCapD3,
 }
 
-impl ExtendedCaptability for XHCIUSBSupportedProtocolCap {
+impl ExtendedCapability for XHCIUSBSupportedProtocolCap {
     fn id() -> u8 {
         0x2
     }
 
-    fn header(&self) -> &crate::drivers::pci::extended_caps::GenericCaptability {
+    fn header(&self) -> &crate::drivers::pci::extended_caps::GenericCapability {
         &self.header
     }
 }
 
 impl XHCIUSBSupportedProtocolCap {
-    /// Returns a ZERO-baased port index representing the first port compatible with this captability
+    /// Returns a ZERO-based port index representing the first port compatible with this capability
     pub const fn first_compatible_port(&self) -> u8 {
         self.compatible_port_offset - 1
     }
 
-    /// Returns a ZERO-baased port index representing the last port compatible with this captability
+    /// Returns a ZERO-based port index representing the last port compatible with this capability
     pub const fn last_compatible_port(&self) -> u8 {
         self.first_compatible_port() + self.compatible_port_count - 1
     }
 
-    /// Returns the major revision version of this captability, eg. 3 for USB3
+    /// Returns the major revision version of this capability, eg. 3 for USB3
     pub const fn major_version(&self) -> u8 {
         self.major_revision_version
     }
